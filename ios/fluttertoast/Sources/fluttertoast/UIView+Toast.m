e64f330b4d32fc4172ee7c5bd64b7bb7e2186da2
Ishaq Hassan <ishaquehassan@gmail.com>
2026-07-17 14:20:50 +0000

    fix: replace deprecated UIActivityIndicatorViewStyleWhiteLarge on iOS 13+
    Use UIActivityIndicatorViewStyleLarge with an explicit white color on iOS 13 and up, and wrap the old WhiteLarge constant in a clang diagnostic push/pop for the pre iOS 13 path so neither branch warns.
    
    Closes #535

--- ios/fluttertoast/Sources/fluttertoast/UIView+Toast.m
+++ ios/fluttertoast/Sources/fluttertoast/UIView+Toast.m
@@ -395,7 +395,16 @@ static const NSString * CSToastQueueKey             = @"CSToastQueueKey";
         activityView.layer.shadowOffset = style.shadowOffset;
     }
     
-    UIActivityIndicatorView *activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+    UIActivityIndicatorView *activityIndicatorView;
+    if (@available(iOS 13.0, *)) {
+        activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleLarge];
+        activityIndicatorView.color = [UIColor whiteColor];
+    } else {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+        activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
+#pragma clang diagnostic pop
+    }
     activityIndicatorView.center = CGPointMake(activityView.bounds.size.width / 2, activityView.bounds.size.height / 2);
     [activityView addSubview:activityIndicatorView];
     [activityIndicatorView startAnimating];
